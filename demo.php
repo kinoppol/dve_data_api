6308c3f4bcad0ab0c0786746831c022244fcd6f0
kinoppol <kinoppol@hotmail.com>
2022-06-11 17:44:05 +0700

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,6 +1,6 @@
 <?php
 include_once ('dve_data_api.php');
-$dve_data=new dve_data_api('5ece4797eaf5e','62a451ed2dc5f');
+$dve_data=new dve_data_api('YOUR_API_ID','YOUR_SECRET_KEY');
 
 $data=$dve_data->get_data($data='student',$qkey='studentCode=6021010112');
 //$data=$dve_data->get_data($data='student',$qkey=NULL);
